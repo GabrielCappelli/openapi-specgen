ff1386614036d2ef7f41a0cf2b86a26e1664d58e
GabrielCappelli <6148081+GabrielCappelli@users.noreply.github.com>
2023-01-24 22:53:52 +0000

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="openapi-specgen",
-    version="1.0.0_dev",
+    version="1.0.0",
     description="Generate OpenApi json specification",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -23,6 +23,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     packages=["openapi_specgen"],
     include_package_data=True,
